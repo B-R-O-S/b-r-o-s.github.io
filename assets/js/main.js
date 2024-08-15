c0326dcf65703f27db5f6d0d3619d14b3f151fd4
Nikhil Tiwari <87422608+NikhilT8144@users.noreply.github.com>
2024-08-15 12:41:37 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
+// Smooth Scrolling for Navigation Links
+$(document).ready(function(){
+    $("a.nav-link").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
 
+// Animate on Scroll (AOS) for Additional Animations
+$(window).scroll(function() {
+    $('.animated').each(function(){
+        var pos = $(this).offset().top;
+        var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+            $(this).addClass('fadeInUpCustom');
+        }
+    });
+});
