b68346773c5e493eb2620192839aaa9b24730c96
Nikhil Tiwari <87422608+NikhilT8144@users.noreply.github.com>
2024-08-15 13:14:34 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,15 @@
-// Smooth Scrolling for Navigation Links
-$(document).ready(function(){
-    $("a.nav-link").on('click', function(event) {
+// Smooth scrolling for internal navigation links
+$(document).ready(function () {
+    $('a.nav-link[href*="#"]').on('click', function (event) {
         if (this.hash !== "") {
             event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-                window.location.hash = hash;
-            });
-        }
-    });
-});
+            const hash = this.hash;
 
-// Animate on Scroll (AOS) for Additional Animations
-$(window).scroll(function() {
-    $('.animated').each(function(){
-        var pos = $(this).offset().top;
-        var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-            $(this).addClass('fadeInUpCustom');
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 70
+            }, 800, function () {
+                window.location.hash = hash - 70;
+            });
         }
     });
 });
